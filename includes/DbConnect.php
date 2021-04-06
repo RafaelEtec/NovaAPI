bd6bffa51de913823c3593e941909ddaa1497605
Rafael Goulart <Rafael.Goulart2@etecia.com.br>
2021-04-06 20:02:50 -0300

    termino base

--- includes/DbConnect.php
+++ includes/DbConnect.php
@@ -1,22 +1,32 @@
 <?php 
-	
 
-
-	function connect()
+	class DbConnect
 	{
-		include_once dirname(__FILE__) . '/Constants.php';
-
-		$this->con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-		if (mysqli_connect_errno(){
-			echo "Falha de conexão com o MySQL: "
-				m
+		
+		
+	 private $con;
+		
+		function __construct()
+		{
+	 
 		}
+	 
+	
+		function connect()
+		{
+		
+			include_once dirname(__FILE__) . '/Constants.php';
+	 
+			
+			$this->con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	 
+		
+			if (mysqli_connect_errno()) {
+				echo "Falha de conexão com o MySQL: " . mysqli_connect_error();
+			}
+	 
+			 
+			return $this->con;
+		}
+	 
 	}
-
-
-
-
-
-
- ?>
--- includes/DbOperation.php
+++ includes/DbOperation.php
@@ -1,56 +1,70 @@
-<?php 
-class DbOperation 
+<?php
+ 
+class DbOperation
 {
-	private $con;
+    
+    private $con;
+ 
+ 
+    function __construct()
+    {
+  
+        require_once dirname(__FILE__) . '/DbConnect.php';
+ 
+     
+        $db = new DbConnect();
+ 
 
-	function __construct()
-	{
-		require_once dirname(__FILE__) . '/DbConnect.php';
-
-		$db = new DbConnect();
-		$this->con = $db->connect();
-	}
-
+        $this->con = $db->connect();
+    }
+	
+	
 	function createHero($name, $realname, $rating, $teamaffiliation){
 		$stmt = $this->con->prepare("INSERT INTO heroes (name, realname, rating, teamaffiliation) VALUES (?, ?, ?, ?)");
-		if ($stmt->execute())
-			return true;
-		return false
+		$stmt->bind_param("ssis", $name, $realname, $rating, $teamaffiliation);
+		if($stmt->execute())
+			return true; 			
+		return false;
 	}
-
-	funtion getHeroes(){
+	
+	function getHeroes(){
 		$stmt = $this->con->prepare("SELECT id, name, realname, rating, teamaffiliation FROM heroes");
 		$stmt->execute();
 		$stmt->bind_result($id, $name, $realname, $rating, $teamaffiliation);
-
-		$heroes = array();
-
+		
+		$heroes = array(); 
+		
 		while($stmt->fetch()){
 			$hero  = array();
-			$hero['id'] = $id;
-			$hero['name'] = $name;
-			$hero['realname'] = $realname;
-			$hero['rating'] = $rating;
-			$hero['teamaffiliation'] = $teamaffiliation;
-
-			array_push($heroes, $hero);
+			$hero['id'] = $id; 
+			$hero['name'] = $name; 
+			$hero['realname'] = $realname; 
+			$hero['rating'] = $rating; 
+			$hero['teamaffiliation'] = $teamaffiliation; 
+			
+			array_push($heroes, $hero); 
 		}
-
-		return $heroes;
+		
+		return $heroes; 
 	}
-
+	
+	
 	function updateHero($id, $name, $realname, $rating, $teamaffiliation){
-		$stmt = $this->con->prepare
+		$stmt = $this->con->prepare("UPDATE heroes SET name = ?, realname = ?, rating = ?, teamaffiliation = ? WHERE id = ?");
+		$stmt->bind_param("ssisi", $name, $realname, $rating, $teamaffiliation, $id);
+		if($stmt->execute())
+			return true; 
+		return false; 
 	}
-
+	
+	
 	function deleteHero($id){
-		$stmt = this->con->prepare("DELETE FROM heroes WHERE id = ?");
+		$stmt = $this->con->prepare("DELETE FROM heroes WHERE id = ? ");
 		$stmt->bind_param("i", $id);
-		if ($stmt->execute())
-			return true;
-		return false;
-		}
+		if($stmt->execute())
+			return true; 
+		return false; 
 	}
-}
 
-?>
+	
+}
